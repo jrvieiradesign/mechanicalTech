f4d49c36d6ee42ee6ae67aa633191d01654715f2
Zaqueu Junior <zaqueujunior1998@gmail.com>
2023-03-06 10:34:55 -0300

    created auth guard for validate user

--- api/src/auth/shared/auth.service.ts
+++ api/src/auth/shared/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from './../../users/shared/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(email: string, userPassword: string) {
+    const user = await this.usersService.findOne(email);
+    if (user && user.password === userPassword) {
+      const { _id, name, email } = user;
+      return { id: _id, name, email };
+    }
+
+    return null;
+  }
+}
--- api/src/users/shared/users.service.ts
+++ api/src/users/shared/users.service.ts
@@ -13,6 +13,9 @@ export class UsersService {
   async findAll() {
     return await this.usersRepository.find();
   }
+  async findOne(email: string) {
+    return await this.usersRepository.findOneBy({ email });
+  }
   async store(UserEmail: CreateUserDto) {
     const user = this.usersRepository.create(UserEmail);
     return await this.usersRepository.save(user);
