9523e3165f4f56b7ca66f52d88359f3b5b448c9b
Zaqueu Junior <zaqueujunior1998@gmail.com>
2023-03-15 14:49:07 -0300

    fixed expiration time acess token

--- api/src/auth/shared/auth.service.ts
+++ api/src/auth/shared/auth.service.ts
@@ -65,7 +65,7 @@ export class AuthService {
     const [at, rt] = await Promise.all([
       this.jwtService.signAsync(jwtPayload, {
         secret: this.config.get<string>('AT_SECRET'),
-        expiresIn: '1m',
+        expiresIn: '15m',
       }),
       this.jwtService.signAsync(jwtPayload, {
         secret: this.config.get<string>('RT_SECRET'),
